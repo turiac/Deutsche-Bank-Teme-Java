69387a9f5a4d8042e0164a3b02aa1dbdfb43d5dd
turiac <turiacalexandrudan@gmail.com>
2022-04-16 15:36:20 +0300

    fixed typo issues

--- src/main/java/com/example/tema/repository/OrderRepository.java
+++ src/main/java/com/example/tema/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.example.tema.repository;
 
 import com.example.tema.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/com/example/tema/repository/PaymentRepository.java
+++ src/main/java/com/example/tema/repository/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.example.tema.repository;
 
 import com.example.tema.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
